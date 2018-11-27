a12f8cb9e9c09a5aee3dc66d4232dd98d2db6e0b
henter <henter@henter.me>
2018-11-27 21:36:13 +0800

    WIP session, cookie

---
+++ Zim/Http/Exception/MethodNotAllowedException.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * File MethodNotAllowedException.php
+ * @henter
+ * Time: 2018-11-27 20:27
+ *
+ */
+
+namespace Zim\Http\Exception;
+
+
+/**
+ * The resource was found but the request method is not allowed.
+ *
+ * This exception should trigger an HTTP 405 response in your application code.
+ *
+ * @author Kris Wallsmith <kris@symfony.com>
+ */
+class MethodNotAllowedException extends \RuntimeException
+{
+    protected $allowedMethods = array();
+
+    public function __construct(array $allowedMethods, string $message = null, int $code = 0, \Exception $previous = null)
+    {
+        $this->allowedMethods = array_map('strtoupper', $allowedMethods);
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * Gets the allowed HTTP methods.
+     *
+     * @return array
+     */
+    public function getAllowedMethods()
+    {
+        return $this->allowedMethods;
+    }
+}
